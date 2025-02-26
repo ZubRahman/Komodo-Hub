6f2137144ffd3b2a76aea258c0b3e61375d80d39
ZubRahman <163024202+ZubRahman@users.noreply.github.com>
2025-02-26 13:53:48 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('Index.html')
 
+@app.route('/aboutus')
+def aboutus():
+    return render_template('about.html',methods=['GET','POST'])
+
 if __name__ == '__main__':
     app.run(debug=True)
