d216dfbda38e9c921422aad84c12df21e81aa41e
ZubRahman <163024202+ZubRahman@users.noreply.github.com>
2025-02-26 23:03:05 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def aboutus():
     return render_template('about.html',methods=['GET','POST'])
 
+@app.route('/contactus')
+def contactus():
+    return render_template('ContactUs.html',methods=['GET','POST'])
+
 if __name__ == '__main__':
     app.run(debug=True)
