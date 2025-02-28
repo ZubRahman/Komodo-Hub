5339bc2dcbc6cf074d68ecf95fd0dd1c3b3af6ac
ZubRahman <163024202+ZubRahman@users.noreply.github.com>
2025-02-28 15:24:44 +0000

    Add files via upload

--- static/javascript/Script.js
+++ static/javascript/Script.js
@@ -1,15 +1,4 @@
-<script>
-        function toggleMode() {
-            // Toggle the 'dark-mode' class on the body element
-            document.body.classList.toggle('dark-mode')
-            // Change the icon for toggle
-            const modeIcon = document.querySelector('.mode-toggle img')
-            if (document.body.classList.contains('dark-mode')) {
-                modeIcon.src = '../static/images/Lightmode on.png' // Change to sun icon for light mode
-                modeIcon.alt = 'Toggle Light Mode'
-            } else {
-                modeIcon.src = '../static/images/darkmode.png' // Change to moon icon for dark mode
-                modeIcon.alt = 'Toggle Dark Mode'
-            }
-        }
-</script>
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
